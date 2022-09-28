c9730767302eeebd8a78262c027cfdaa8ab0f351
ShintaroTakahashi <shintaro_takahashi@bita.jp>
2022-09-28 17:53:41 +0900

    Updates

---
+++ 745.4a3a2e49.iframe.bundle.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunklink_storybook_with_figma=self.webpackChunklink_storybook_with_figma||[]).push([[745],{"./node_modules/react-dom/client.js":(__unused_webpack_module,exports,__webpack_require__)=>{var m=__webpack_require__("./node_modules/react-dom/index.js");exports.createRoot=m.createRoot,exports.hydrateRoot=m.hydrateRoot}}]);
